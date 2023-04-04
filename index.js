80fac5f3610a6b8aff39ea7121093f20b807fabb
Federico <fritzmaximovicz@gmail.com>
2023-04-04 13:40:34 -0300

    Exercises 3.12-3.14

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
-
+require('dotenv').config()
 const express = require('express')
 const morgan = require('morgan')
 const cors = require('cors')
+const Person = require('./models/person')
 
 const app = express()
 
@@ -13,31 +14,33 @@ morgan.token('data', (req)=>{
 })
 app.use(morgan(`:method :url :status :res[content-length] - :response-time ms :data`))
 
-let persons = [
-    { 
-      "id": 1,
-      "name": "Arto Hellas", 
-      "number": "040-123456"
-    },
-    { 
-      "id": 2,
-      "name": "Ada Lovelace", 
-      "number": "39-44-5323523"
-    },
-    { 
-      "id": 3,
-      "name": "Dan Abramov", 
-      "number": "12-43-234345"
-    },
-    { 
-      "id": 4,
-      "name": "Mary Poppendieck", 
-      "number": "39-23-6423122"
-    }
-]
+// let persons = [
+//     { 
+//       "id": 1,
+//       "name": "Arto Hellas", 
+//       "number": "040-123456"
+//     },
+//     { 
+//       "id": 2,
+//       "name": "Ada Lovelace", 
+//       "number": "39-44-5323523"
+//     },
+//     { 
+//       "id": 3,
+//       "name": "Dan Abramov", 
+//       "number": "12-43-234345"
+//     },
+//     { 
+//       "id": 4,
+//       "name": "Mary Poppendieck", 
+//       "number": "39-23-6423122"
+//     }
+// ]
 
 app.get('/api/persons', (req, res) => {
-    res.json(persons)
+    Person.find({}).then(persons => {
+        res.json(persons)
+    })
 })
 
 app.get('/info', (req, res) => {
@@ -48,9 +51,12 @@ app.get('/info', (req, res) => {
 })
 
 app.get('/api/persons/:id', (req, res) => {
-    const id = Number(req.params.id)
-    const person = persons.find(person => person.id === id)
-    person ? res.json(person) : res.status(404).end()
+    Person.findById(req.params.id).then(person=>{
+        person ? res.json(person) : res.status(404).end()
+    }).catch(err => {
+        console.log(err)
+        res.status(400).send({error: 'the id format is incorrect'})
+    })
 })
 
 app.delete('/api/persons/:id', (req, res) => {
@@ -58,18 +64,19 @@ app.delete('/api/persons/:id', (req, res) => {
     persons = persons.filter(person => person.id !== id)
 
     res.status(204).end()
+
 })
 
-const personExists = (name) => {
-    let alreadyExists = false
+// const personExists = (name) => {
+//     let alreadyExists = false
 
-    persons.forEach(p => {
-        if(p.name === name){
-            alreadyExists = true
-        }
-    })
-    return alreadyExists
-}
+//     persons.forEach(p => {
+//         if(p.name === name){
+//             alreadyExists = true
+//         }
+//     })
+//     return alreadyExists
+// }
 
 app.post('/api/persons', (req, res) => {
     const body = req.body
@@ -80,24 +87,25 @@ app.post('/api/persons', (req, res) => {
         })
     }
 
-    if(personExists(body.name)){
-        return res.status(400).json({
-            error: "This person's name already exists, each new person must have a unique name"
-        })
-    }
+    // if(personExists(body.name)){
+    //     return res.status(400).json({
+    //         error: "This person's name already exists, each new person must have a unique name"
+    //     })
+    // }
 
-    const person = {
-        id: Math.floor(Math.random()*100000),
+    const person = new Person({
         name: body.name,
-        number: body.number,
-    }
+        number: body.number
+    })
 
-    persons = persons.concat(person)
-    res.json(person)
+    
+    person.save().then(savedPerson => {
+        res.json(savedPerson)
+    })
 })
 
 
-const PORT = process.env.PORT || 3001
+const PORT = process.env.PORT
 
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`)
